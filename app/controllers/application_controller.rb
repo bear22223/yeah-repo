526798931dc4a1b079e06e5e3807b42081bdf2f4
bear2223 <8ujhsjdw@gmail.com>
2023-06-10 19:58:37 +0000

    Finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
+  def set_user
+    if params[:id]
+      @user = User.find(params[:id])
+    elsif params[:user_id]
+      @user = User.find(params[:user_id])
+    end
+  end
+  
   def logged_in_user
     unless logged_in?
       store_location
